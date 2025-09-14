53298e3580cdc4dcbff3686ed9546a948b685d3f
redpanda577 <51419294+redpanda577@users.noreply.github.com>
2025-09-14 11:33:42 -0600

    refactor and add query class, pls test

--- PopulateMongoDB/src/main/java/com/cs314/App.java
+++ PopulateMongoDB/src/main/java/com/cs314/App.java
@@ -1,24 +1,4 @@
 package com.cs314;
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Indexes;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
-
-import org.bson.Document;
 
 public class App {
 
@@ -26,66 +6,9 @@ public class App {
         System.out.println();System.out.println();System.out.println();
         System.out.println("HELLO");
         System.out.println();System.out.println();System.out.println();
-        int localPort = 27017;
-        String connection = "mongodb://localhost:27017";
-        String csvPath = args[0];
         
-        // Connect to db
-        try (MongoClient mongoClient = MongoClients.create(connection)) {
-            MongoDatabase database = mongoClient.getDatabase("cs314");
-            MongoCollection<Document> collection = database.getCollection("cities");
-            
-            // Open csv
-            Path path = Paths.get(csvPath);
-            try (BufferedReader reader = Files.newBufferedReader(path)) {
-                String line = reader.readLine();
-                String[] fields = processLine(line);
-                System.out.println();System.out.println();System.out.println();
-                System.out.println(Arrays.toString(fields));
-                System.out.println();System.out.println();System.out.println();
-                List<Document> batch = new ArrayList<>();
-                int batchCap = 1000;
-
-                while((line = reader.readLine()) != null) {
-                    String[] values = processLine(line);
-                    batch.add(buildDoc(fields, values));
-                    if (batch.size() == batchCap) {
-                        collection.insertMany(batch);
-                        batch.clear();
-                    }                   
-                }
-                if (batch.size() > 0) {
-                    collection.insertMany(batch);
-                }
-                createIndexes(collection, fields);
-            }
-            catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
-        }
-    }
-
-    private static String[] processLine(String line) {
-        String[] values = line.split(",");
-        for (int i = 0; i < values.length; i++) {
-            values[i] = values[i].replace("\"", "");
-        }
-        return values;
-    }
-    
-    private static Document buildDoc(String[] fields, String[] values) {
-        Document doc = new Document();
-        for (int i = 0; i < fields.length; i++) {
-            doc.append(fields[i], values[i]);
-        }
-        return doc;
-    }
-
-    private static void createIndexes(MongoCollection<Document> collection, String[] fields) {
-        for (int i = 0; i < fields.length; i++) {
-            String key = fields[i];
-            collection.createIndex(Indexes.ascending(key));
-        }
+        String csvPath = args[0];
+        Populate c = new Populate(csvPath);
     }
 
 }
---
+++ PopulateMongoDB/src/main/java/com/cs314/Query.java
@@ -0,0 +1,44 @@
+package com.cs314;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+public class Query {
+    private MongoCollection<Document> collection;
+
+    public Query(){
+        try (MongoClient mongoClient = MongoClients.create(Database.connection)) {
+            MongoDatabase database = mongoClient.getDatabase("cs314");
+            collection = database.getCollection("cities");
+        } 
+        catch (Exception e) {
+                System.err.println(e.getMessage());
+        }
+    }
+
+    public void getCitiesByRegex(String field, String regex){
+        Bson filter = Filters.regex(field, regex);
+        // wrapper class for FindIterable<Documents> called QueryResult?
+        FindIterable<Document> results = collection.find(filter);
+        results.forEach(doc -> System.out.println(doc.toJson()));
+    }
+
+    private Bson between(String field, double min, double max){
+        return Filters.and(Filters.gt(field, min), Filters.lt(field, max));
+    }
+
+    public void getCitiesInRange(String field, double min, double max){
+        Bson filter = between(field, min, max);
+        FindIterable<Document> results = collection.find(filter);
+        results.forEach(doc -> System.out.println(doc.toJson()));
+    }
+
+    // etc.
+}
