786f41bc8ff94a908c39a278544b4523adfee426
ennskevin <ennskevin@tutanota.com>
2025-09-14 15:54:04 -0600

    fix typo

--- PopulateMongoDB/src/main/java/com/cs314/Database.java
+++ PopulateMongoDB/src/main/java/com/cs314/Database.java
@@ -1,6 +1,6 @@
 package com.cs314;
 
-public class Database {
+public final class Database {
     public static final int localPort = 27017;
     public static final String connection = String.format("mongodb://localhost:%d", localPort);
 }
