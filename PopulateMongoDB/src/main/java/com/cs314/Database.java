08173cd6c2728ec5a9cc4ce4a92864eac032a21d
ennskevin <ennskevin@tutanota.com>
2025-09-14 16:07:19 -0600

    Remove PopulateMongoDB directory

--- PopulateMongoDB/src/main/java/com/cs314/Database.java
+++
@@ -1,6 +0,0 @@
-package com.cs314;
-
-public final class Database {
-    public static final int localPort = 27017;
-    public static final String connection = String.format("mongodb://localhost:%d", localPort);
-}
