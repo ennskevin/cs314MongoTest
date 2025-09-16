9ccb010a43d195f2a3b3cf0f08e86261055dd4ed
ennskevin <ennskevin@tutanota.com>
2025-09-16 09:38:33 -0600

    Add user and password fields to database class

--- LoadMongoDB/src/main/java/com/cs314/Database.java
+++ LoadMongoDB/src/main/java/com/cs314/Database.java
@@ -3,4 +3,6 @@ package com.cs314;
 public final class Database {
     public static final int localPort = 27017;
     public static final String connection = String.format("mongodb://localhost:%d", localPort);
+    public static final String user = "readWriter";
+    public static final String password = "password";
 }
