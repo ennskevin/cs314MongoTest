4bcd1976d9092b193380e4720dfd1d560251146d
ennskevin <ennskevin@tutanota.com>
2025-09-16 10:01:48 -0600

    Update connection string in Database.java to use user auth

--- QueryMongoDB/src/main/java/com/cs314/Database.java
+++ QueryMongoDB/src/main/java/com/cs314/Database.java
@@ -1,6 +1,11 @@
 package com.cs314;
 
-public class Database {
-    public static final int localPort = 27017;
-    public static final String connection = String.format("mongodb://localhost:%d", localPort);
+public final class Database {
+    public static final int port = 27017;
+    public static final String user = "reader";
+    public static final String password = "password";
+    public static final String name = "cs314";
+    public static final String collection = "cities";
+
+    public static final String connection = String.format("mongodb://%s:%s@localhost:%d/?authSource=admin", user,password,port);
 }
