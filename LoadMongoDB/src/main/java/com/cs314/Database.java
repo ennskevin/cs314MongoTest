d003dcec12b7820faa8ec9e4e963b7b0822a77cd
ennskevin <ennskevin@tutanota.com>
2025-09-16 09:40:11 -0600

    Update Database fields

--- LoadMongoDB/src/main/java/com/cs314/Database.java
+++ LoadMongoDB/src/main/java/com/cs314/Database.java
@@ -1,8 +1,10 @@
 package com.cs314;
 
 public final class Database {
-    public static final int localPort = 27017;
+    public static final int port = 27017;
     public static final String connection = String.format("mongodb://localhost:%d", localPort);
     public static final String user = "readWriter";
     public static final String password = "password";
+    public static final String name = "cs314";
+    public static final String collection = "cities";
 }
